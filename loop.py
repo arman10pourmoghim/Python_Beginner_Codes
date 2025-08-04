c7cd2abb7d7a9bb04f62914f3376dbc8d80c20c8
arman10pourmoghim <armanpr0710goat@gmail.com>
2025-08-04 14:27:03 +0330

    while vs. for
    counting from 0 to 10

---
+++ loop.py
@@ -0,0 +1,12 @@
+print("While loop vs. For loop")
+print("Counting from 0 to 10:")
+
+print("\nWhile loop:")
+i= 0
+while i <= 10:
+    print(i)
+    i += 1
+
+print("\nFor loop:")
+for j in range(11):
+    print(j)
